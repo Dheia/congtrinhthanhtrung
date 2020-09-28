b555735182a9ec347fffdeeceacff668838ac069
baoch <baoch2310@gmail.com>
2020-09-29 01:45:24 +0700

    [UPDATE] split branch to test

---
+++ storage/cms/cache/82/6a/footer.htm.php
@@ -0,0 +1,5 @@
+<?php 
+class Cms5f6e51ca0faf8461225554_23042169a4ecc25a36b246961d1c3a45Class extends Cms\Classes\PartialCode
+{
+
+}
---
+++ storage/cms/cache/b9/7d/home.htm.php
@@ -0,0 +1,5 @@
+<?php 
+class Cms5f6e51c9ebcce641253442_45c3a39f1e945bceaa371388aa30e257Class extends Cms\Classes\PageCode
+{
+
+}
