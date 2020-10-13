7885abb158c1414414877a289a8b1dbad10e1de1
baoch <chuybao@tma.com.vn>
2020-10-14 00:38:11 +0700

    [UPDATE] clear cache

--- storage/cms/cache/91/c8/header.htm.php
+++
@@ -1,5 +0,0 @@
-<?php 
-class Cms5f7eecb9a5ade473447257_92952f045a6a832fd4eb44d5c153cbebClass extends Cms\Classes\PartialCode
-{
-
-}
--- storage/cms/cache/b9/7d/home.htm.php
+++
@@ -1,5 +0,0 @@
-<?php 
-class Cms5f7eecb995975488220433_ca38bfa38837e61d0ce5eb7f49860b6eClass extends Cms\Classes\PageCode
-{
-
-}
